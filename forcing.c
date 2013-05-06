b1d0cbe860dde279e0534c4f971e9d21f9aa262d
nvcleemp <nico.vancleemput@gmail.com>
2013-05-06 21:32:55 +0200

    First version of forcing.c

---
+++ forcing.c
@@ -0,0 +1,294 @@
+
+#include <stdio.h>
+#include <getopt.h>
+#include "cliquer.h"
+#include "freqtable.h"
+
+int graphCount;
+
+freqtable forcingNumberCount = NULL;
+
+freqtable independentSetCounts = NULL;
+
+int onlyGraph = 0;
+int detailed = FALSE;
+
+//===================================================================
+// Forcing independence methods
+//===================================================================
+
+set_t core;
+set_t anticore;
+int independentSetCount;
+int alpha;
+int forcingNumber;
+
+
+boolean handleClique(set_t s,graph_t *g,clique_options *opt){
+    if(detailed){
+        set_print(s);
+    }
+    if(independentSetCount==0){
+        //use the first independent set to determine the value of alpha
+        alpha = set_size(s);
+    }
+    independentSetCount++;
+    
+    set_t oldCore = core;
+    core = set_intersection(NULL, oldCore, s);
+    set_free(oldCore);
+    
+    set_t oldAnticore = anticore;
+    anticore = set_union(NULL, oldAnticore, s);
+    set_free(oldAnticore);
+    
+    return TRUE;
+}
+
+void processGraph(graph_t *g){
+    clique_options *opts;
+    opts=malloc(sizeof(clique_options));
+    opts->time_function=NULL;
+    opts->output=stderr;
+    opts->reorder_function=NULL;
+    opts->reorder_map=NULL;
+    opts->user_function=handleClique;
+    opts->user_data=NULL;
+    opts->clique_list=NULL;
+    opts->clique_list_length=0;
+    
+    core = set_new(g->n);
+    anticore = set_new(g->n);
+    int i;
+    for (i=0; i<g->n; i++){
+        SET_ADD_ELEMENT(core, i);
+    }
+    set_empty(anticore);
+    independentSetCount = 0;
+    forcingNumber = -1;
+    
+    clique_find_all(g,0,0,TRUE,opts);
+    
+    fprintf(stdout, "%d, %d, %d, %d, %d, %d\n", graphCount, g->n, alpha, independentSetCount, set_size(core), g->n - set_size(anticore));
+    
+    if(alpha != g->n/2) return;
+    
+    independentSetCounts = increment(independentSetCounts, independentSetCount);
+    
+    if(independentSetCount == 1){
+        forcingNumber = 0;
+    } else if(independentSetCount == 2){
+        forcingNumber = 1;
+    }
+    
+    forcingNumberCount = increment(forcingNumberCount, forcingNumber);
+    
+    set_free(core);
+    set_free(anticore);
+}
+
+//===================================================================
+// Graph processing methods
+//===================================================================
+
+graph_t *getComplement(graph_t *g){
+    int i, j;
+    graph_t * gComplement = graph_new(g->n);
+    for(i = 0; i < g->n-1; i++){
+        for(j = i+1; j < g->n; j++){
+            if(!GRAPH_IS_EDGE_FAST(g,i,j)){
+                GRAPH_ADD_EDGE(gComplement, i, j);
+            }
+        }
+    }
+    return gComplement;
+}
+
+//===================================================================
+// Printing methods
+//===================================================================
+
+void printSet(set_t s, graph_t *g, clique_options *opt){
+    set_print(s);
+}
+
+//===================================================================
+// Input methods
+//===================================================================
+boolean readPlanarCode(FILE *f, graph_t **g){
+    static int first = 1;
+    unsigned char c;
+    char testheader[20];
+    int zeroCounter;
+    
+    if (first) {
+        first = 0;
+
+        if (fread(&testheader, sizeof (unsigned char), 15, f) != 15) {
+            fprintf(stderr, "can't read header ((1)file too small)-- exiting\n");
+            exit(1);
+        }
+        testheader[15] = 0;
+        if (strcmp(testheader, ">>planar_code<<") != 0) {
+            fprintf(stderr, "No planarcode header detected -- exiting!\n");
+            exit(1);
+        }
+    }
+
+    if (fread(&c, sizeof (unsigned char), 1, f) == 0) {
+        //nothing left in file
+        return (0);
+    }
+
+    if (c != 0) /* unsigned chars would be sufficient */ {
+        int order = c;
+        *g = graph_new(order);
+        zeroCounter = 0;
+        while (zeroCounter < order) {
+            int neighbour = (unsigned short) getc(f);
+            if (neighbour == 0) {
+                zeroCounter++;
+            } else {
+                GRAPH_ADD_EDGE(*g, zeroCounter, neighbour-1);
+            }
+        }
+    } else {
+        int order = 0;
+        int readCount = fread(&order, sizeof (unsigned short), 1, f);
+        if(!readCount){
+            fprintf(stderr, "Unexpected EOF.\n");
+            exit(1);
+        }
+        *g = graph_new(order);
+        int neighbour = 0;
+        zeroCounter = 0;
+        while (zeroCounter < order) {
+            readCount = fread(&neighbour, sizeof (unsigned short), 1, f);
+            if(!readCount){
+                fprintf(stderr, "Unexpected EOF.\n");
+                exit(1);
+            }
+            if (neighbour == 0) {
+                zeroCounter++;
+            } else {
+                GRAPH_ADD_EDGE(*g, zeroCounter, neighbour-1);
+            }
+        }
+    }
+
+    return (1);
+}
+
+//===================================================================
+// Usage methods
+//===================================================================
+void help(char *name){
+    fprintf(stderr, "The program %s calculates the forcing number for graphs.\n\n", name);
+    fprintf(stderr, "Usage\n=====\n");
+    fprintf(stderr, " %s [options]\n", name);
+    fprintf(stderr, "       Calculate the forcing number for any graph read through stdin.\n");
+    fprintf(stderr, "\n\n");
+    fprintf(stderr, "Currently only graphs in planarcode format are supported.");
+    fprintf(stderr, "\n\n");
+    fprintf(stderr, "Valid options\n=============\n");
+    fprintf(stderr, "* Various options\n");
+    fprintf(stderr, "    -h, --help\n");
+    fprintf(stderr, "       Print this help and return.\n");
+    fprintf(stderr, "    -f, --filter n\n");
+    fprintf(stderr, "       Only do the calculations for the nth graph in the input.\n");
+    fprintf(stderr, "    -d, --detailed\n");
+    fprintf(stderr, "       Print various details during the calculations.\n");
+}
+
+void usage(char *name){
+    fprintf(stderr, "Usage: %s [options]\n", name);
+    fprintf(stderr, "For more information type: %s -h \n\n", name);
+}
+
+/*
+ * process any command-line options.
+ */
+boolean processOptions(int argc, char **argv) {
+    int c;
+    char *name = argv[0];
+    static struct option long_options[] = {
+        {"help", no_argument, NULL, 'h'},
+        {"filter", required_argument, NULL, 'f'},
+        {"detailed", no_argument, NULL, 'd'}
+    };
+    int option_index = 0;
+
+    while ((c = getopt_long(argc, argv, "hf:d", long_options, &option_index)) != -1) {
+        switch (c) {
+            case 0:
+                //handle long option with no alternative
+                switch(option_index) {
+                    default:
+                        fprintf(stderr, "Illegal option index %d.\n", option_index);
+                        usage(name);
+                        return EXIT_FAILURE;
+                }
+                break;
+            case 'h':
+                help(name);
+                return EXIT_SUCCESS;
+            case 'f':
+                onlyGraph = atoi(optarg);
+                break;
+            case 'd':
+                detailed = TRUE;
+                break;
+            case '?':
+                usage(name);
+                return EXIT_FAILURE;
+            default:
+                fprintf(stderr, "Illegal option %c.\n", c);
+                usage(name);
+                return EXIT_FAILURE;
+        }
+    }
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char *argv[]) {
+    graph_t *g = NULL;
+    graph_t *gComplement;
+//    set_t s;
+    
+    if(processOptions(argc, argv)==EXIT_FAILURE) return EXIT_FAILURE;
+    
+    graphCount = 0;
+    
+    while (readPlanarCode(stdin, &g)) {
+        graphCount++;
+        
+        //filtering
+        if(onlyGraph && graphCount != onlyGraph) continue;
+        
+        ASSERT(graph_test(g,stderr));
+        gComplement = getComplement(g);
+        
+        if(detailed) graph_print(g);
+        
+        processGraph(gComplement);
+        
+        graph_free(g);
+        graph_free(gComplement);
+    }
+    
+    char *format = "%5d : %6d\n";
+    
+    fprintf(stderr, "Count   Graphs\n");
+    fprintf(stderr, "--------------\n");
+    printFrequencyTable(independentSetCounts, format);
+    
+    format = "%3d : %6d\n";
+    fprintf(stderr, " f    Graphs\n");
+    fprintf(stderr, "------------\n");
+    printFrequencyTable(forcingNumberCount, format);
+    
+    fprintf(stderr, "Read %d graphs.\n", graphCount);
+
+    return 0;
+}
+
